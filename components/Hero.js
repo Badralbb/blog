c1f18d8a331981fb1223f1ac4db3a250365070e5
Badralbb <badralbaigalmaa7@gmail.com>
2024-07-24 14:34:29 +0800

    1

--- components/Hero.js
+++ components/Hero.js
@@ -11,12 +11,13 @@ export const Hero = () => {
         })
     }, [])
     return (
-        <div className="flex gap-2 flex-wrap w-full max-w-[1216px] mx-auto">
+        <div className="grid grid-cols-1 gap-2 w-full max-w-[1216px] mx-auto md:grid-cols-3">
             {articles.map(item => (
-                <div key={item.id} className="p-4 max-w-[396px] w-full border-[1px] border-solid border-[#E8E8EA] rounded-xl">
+                <div key={item.id} className="p-4 max-w-[396px] mx-auto w-full border-[1px] border-solid border-[#E8E8EA] rounded-xl">
  
                     <div className="rounded-lg mb-">
-                        <Image width={100} height={100} src={item.cover_image} />
+                        {/* <Image width={100} height={100} src={item.cover_image} /> */}
+                        <img src={item.cover_image}/>
                     </div>
                     <div className="pt-4">
                         <div className="mb-5">
