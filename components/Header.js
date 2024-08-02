6da3b8b44a579a4a41c84ec29b7536e67d40c181
Badralbb <badralbaigalmaa7@gmail.com>
2024-08-02 22:00:03 +0800

    1

--- components/Header.js
+++ components/Header.js
@@ -8,13 +8,14 @@ export const Header = () => {
   const [article, setArticle] = useState([]);
 
   useEffect(() => {
-    fetch("https://dev.to/api/articles?username=ben").then((response) => {
-      return response.json();
-    }).then((data) => {
-      setArticle(data);
-    });
+    fetch("https://dev.to/api/articles?username=ben")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticle(data);
+      });
   }, []);
-
 
   return (
     <div>
@@ -23,44 +24,51 @@ export const Header = () => {
           All Blog Post
         </div>
         <div className="carousel w-full aspect-[4/2]">
-          {
-            article.map((item, index) => (
-              <div key={item.id} id={`slide${index}`} className="carousel-item relative w-full">
-                <Image width={2000} height={2000}
-                  src={item.cover_iamge || item.social_image}
-                  className="w-full rounded-xl" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                  <Link href={`#slide${index - 1}`} className="btn btn-circle">❮</Link>
-                  <Link href={`#slide${index + 1}`} className="btn btn-circle">❯</Link>
-                </div>
-                <div className="absolute p-10 left-2 bottom-2 bg-[#E8E8EA] rounded-xl max-w-[30%] w-full">
-
-                  <div className="badge badge-primary">{item.tag_list[0]}</div>
-                  <div>{item.description}</div>
-                  <div>{dayjs(item.published_at).format("YYYY/MM/d")}</div>
-                </div>
+          {article.map((item, index) => (
+            <div
+              key={item.id}
+              id={`slide${index}`}
+              className="carousel-item relative w-full"
+            >
+              <Image
+                width={2000}
+                height={2000}
+                src={item.cover_iamge || item.social_image}
+                className="w-full rounded-xl"
+              />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <Link href={`#slide${index - 1}`} className="btn btn-circle">
+                  ❮
+                </Link>
+                <Link href={`#slide${index + 1}`} className="btn btn-circle">
+                  ❯
+                </Link>
               </div>
-            ))
-          }
-
-        </div>
-
-      </div>
-      <div className="carousel md:hidden carousel-center bg-neutral rounded-box items-center space-x-4 p-4 ">
-        {
-          article.map(item => (
-            <div className="carousel-item">
-              <Image width={500} height={500}
-                src={item.social_image}
-                className="rounded-box w-full" />
+              <div className="absolute p-10 left-2 bottom-2 bg-[#E8E8EA] rounded-xl max-w-[30%] w-full">
+                <div className="badge badge-primary">{item.tag_list[0]}</div>
+                <div>{item.description}</div>
+                <div>{dayjs(item.published_at).format("YYYY/MM/d")}</div>
+              </div>
             </div>
           ))}
+        </div>
+      </div>
+      <div className="carousel md:hidden carousel-center bg-neutral rounded-box items-center space-x-4 p-4 ">
+        {article.map((item) => (
+          <div key={item.id} className="carousel-item">
+            <Image
+              width={500}
+              height={500}
+              src={item.social_image}
+              className="rounded-box w-full"
+            />
+          </div>
+        ))}
       </div>
       <div className="max-w-[1216px] mx-auto">
         <h3 className="mb-8">Trending</h3>
 
         <div className="hidden md:flex gap-5 mb-28">
-
           {article.map(
             (item, index) =>
               index < 4 && (
@@ -68,15 +76,13 @@ export const Header = () => {
                   key={item.id}
                   className="max-w-[293px] w-full relative overflow-hidden"
                 >
-
                   <Image
                     className="w-full rounded-xl"
                     src={item.user.profile_image}
                     width={100}
                     height={100}
                   />
-                  <div className="bg-[#141624] w-full absolute inset-0 opacity-50 rounded-xl">
-                  </div>
+                  <div className="bg-[#141624] w-full absolute inset-0 opacity-50 rounded-xl"></div>
                   <div className="p-7 absolute w-full bottom-0 left-0 ">
                     <div className="badge badge-primary mb-4">
                       {item.tag_list[0]}
@@ -91,14 +97,22 @@ export const Header = () => {
         </div>
         <div className="block md:hidden ">
           <div className="carousel carousel-vertical rounded-box h-96">
-            {
-              article.map((item, index) => (index < 4 &&
-
-                <div key={item.id} className="carousel-item h-full max-w-[80%] w-full mx-auto">
-                  <Image className="w-full rounded-3xl" src={item.social_image} width={100} height={100} />
-                </div>
-              ))
-            }
+            {article.map(
+              (item, index) =>
+                index < 4 && (
+                  <div
+                    key={item.id}
+                    className="carousel-item h-full max-w-[80%] w-full mx-auto"
+                  >
+                    <Image
+                      className="w-full rounded-3xl"
+                      src={item.social_image}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                )
+            )}
           </div>
         </div>
       </div>
--- components/Hero.js
+++ components/Hero.js
@@ -4,6 +4,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "@/components/mn";
 import Link from "next/link";
+import { name } from "@/components/mn";
 dayjs.extend(relativeTime);
 const perPage = 5;
 export const Hero = () => {
@@ -14,64 +15,73 @@ export const Hero = () => {
 
   let nextPage = 1;
   const [tag, setTag] = useState("");
-  const tags = [{ name: "all", value: "" }
-    , { name: "Front-end", value: "frontend" },
-  { name: "Web-Dev", value: "webdev" }
-    , { name: "Анхан шат", value: "beginners" },
-  { name: "JS", value: "javascript" }, {
-    name: "CSS", value: "css"
-  }, {
-    name: 'HTML', value: "html"
-  }, {
-    name: 'saas', value: "saas"
-  },
-  {
-    name: 'portfolio', value: "portfolio"
-  },
-    , { name: "programming", value: "programming" }
-  ]
-  const viewAll = ["viewall"]
-
-
-
+  const tags = [
+    { name: "all", value: "" },
+    { name: "Front-end", value: "frontend" },
+    { name: "Web-Dev", value: "webdev" },
+    { name: "Анхан шат", value: "beginners" },
+    { name: "JS", value: "javascript" },
+    {
+      name: "CSS",
+      value: "css",
+    },
+    {
+      name: "HTML",
+      value: "html",
+    },
+    {
+      name: "saas",
+      value: "saas",
+    },
+    {
+      name: "portfolio",
+      value: "portfolio",
+    },
+    ,
+    { name: "programming", value: "programming" },
+  ];
+  const viewAll = { name: "viewall", value: "viewAll" };
+  async function AllView() {
+    setEnded(true);
+    const response = await fetch(
+      `https://dev.to/api/articles?username=paul_freeman&tag=${tag}`
+    );
+    const data = await response.json();
+    setArticles(data);
+  }
   async function loadmore() {
-    nextPage = page + 1
+    nextPage = page + 1;
     setPage(page + 1);
-    console.log(page)
+    console.log(page);
     setLoading(true);
-    const response = await fetch(`https://dev.to/api/articles?username=paul_freeman&page=${nextPage}&per_page=${perPage}`)
-    const data = await response.json()
+    const response = await fetch(
+      `https://dev.to/api/articles?username=paul_freeman&page=${nextPage}&per_page=${perPage}`
+    );
+    const data = await response.json();
     if (data.length < perPage) {
       setEnded(true);
     }
     setLoading(false);
-    setArticles(articles.concat(data))
-
+    setArticles(articles.concat(data));
   }
 
-
   async function FilterArticle() {
-    setPage(nextPage)
-    nextPage = 1
-    const response = await fetch(`https://dev.to/api/articles?username=paul_freeman&page=${nextPage}&per_page=${perPage}&tag=${tag}`);
+    setPage(nextPage);
+    nextPage = 1;
+    const response = await fetch(
+      `https://dev.to/api/articles?username=paul_freeman&page=${nextPage}&per_page=${perPage}&tag=${tag}`
+    );
     const data = await response.json();
-    let UpdatedArticles = await data
+    let UpdatedArticles = await data;
     setArticles(UpdatedArticles);
     setLoading(false);
-
-  };
+  }
 
   useEffect(() => {
-
     FilterArticle();
   }, [tag]);
   if (!articles.length) {
-    return (
-      <div>
-        loading
-      </div>
-    )
-
+    return <div>loading</div>;
   }
 
   return (
@@ -81,67 +91,63 @@ export const Hero = () => {
       </div>
 
       <div>
-
         <div className="flex justify-between">
-
           <div className="flex gap-5 mb-8">
-            {
-              tags.map(item => (
-
-                <div key={item.value} onClick={() => setTag(item.value)} className={`hover:text-yellow-400 cursor-pointer font-bold ${tag === item.value ? "text-yellow-300" : ""}`}>
-                  {item.name}
-                </div>
-              ))
-            }
+            {tags.map((item) => (
+              <div
+                key={item.value}
+                onClick={() => setTag(item.value)}
+                className={`hover:text-yellow-400 cursor-pointer font-bold ${
+                  tag === item.value ? "text-yellow-300" : ""
+                }`}
+              >
+                {item.name}
+              </div>
+            ))}
           </div>
-          <div className={`hover:text-yellow-400 cursor-pointer font-bold`} >
-            {viewAll[0]}
+          <div
+            className={`hover:text-yellow-400 cursor-pointer font-bold`}
+            onClick={AllView}
+          >
+            {viewAll.name}
           </div>
         </div>
         <div className="grid grid-cols-1 gap-2 w-full mx-auto md:grid-cols-3">
+          {articles.map((item) => (
+            <div
+              key={item.id}
+              className="p-4 max-w-[396px] mx-auto w-full border-[1px] border-solid border-[#E8E8EA] rounded-xl shadow-lg"
+            >
+              <div className="mb-4 max-w-[360px]">
+                <Link href={item.path}>
+                  <Image
+                    width={1000}
+                    height={1000}
+                    src={item.cover_image || item.social_image}
+                    className="w-full rounded-lg aspect-video object-cover bg-slate-600"
+                  />
+                </Link>
+              </div>
 
-          {
-            articles.map(item =>
-              <div
-                key={item.id}
-                className="p-4 max-w-[396px] mx-auto w-full border-[1px] border-solid border-[#E8E8EA] rounded-xl shadow-lg"
-              >
-                <div className="mb-4 max-w-[360px]">
-                  <Link href={item.path}>
-                    <Image
-                      width={1000}
-                      height={1000}
-                      src={item.cover_image || item.social_image}
-                      className="w-full rounded-lg aspect-video object-cover bg-slate-600"
-                    />
-                  </Link>
+              <div className="pt-4">
+                <div className="mb-5">
+                  <div className="mb-4">
+                    {dayjs(item.published_at).locale("mn").fromNow()}
+                  </div>
+                  <div className="badge badge-primary">
+                    {tag !== ""
+                      ? item.tag_list.map((item) => item == tag && item)
+                      : item.tag_list[0]}
+                  </div>
+                  <div>{item.description}</div>
                 </div>
-
-                <div className="pt-4">
-                  <div className="mb-5">
-                    <div className="mb-4">
-                      {dayjs(item.published_at).locale("mn").fromNow()}
-                    </div>
-                    <div className="badge badge-primary">
-
-                      {
-                        tag !== "" ?
-                          item.tag_list.map(item => (
-                            item == tag && item
-                          )) : item.tag_list[0]}
-
-                    </div>
-                    <div>{item.description}</div>
-                  </div>
-                  <div className="flex gap-2 items-center">
-                    <Image src={item.user.profile_image} width={50} height={50} />
-                    <div>{item.user.username}</div>
-                  </div>
+                <div className="flex gap-2 items-center">
+                  <Image src={item.user.profile_image} width={50} height={50} />
+                  <div>{item.user.username}</div>
                 </div>
               </div>
-            )}
-
-
+            </div>
+          ))}
         </div>
       </div>
       {!ended && (
