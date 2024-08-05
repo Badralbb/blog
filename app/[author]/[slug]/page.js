c159b57afef9509fe82fce421390b71fd3e6dc32
Badralbb <badralbaigalmaa7@gmail.com>
2024-08-05 15:55:27 +0800

    1

---
+++ app/[author]/[slug]/page.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+import parse from 'html-react-parser';
+
+
+
+async function Page({ params }) {
+
+    const response = await fetch(`https://dev.to/api/articles/${params.author}/${params.slug}`);
+    const detail = await response.json()
+
+
+}
+
+
+
+return (
+    <div>
+
+
+        <div className="prose w-[80%] mx-auto">
+            <div>{parse(article.title)}</div>
+            <div>{parse(article.body_html)}</div>
+        </div>
+    </div>
+
+)
+
