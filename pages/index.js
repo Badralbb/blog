692e6bf59796982cfd32432e37d68e21d85180a0
Badralbb <badralbaigalmaa7@gmail.com>
2024-08-05 21:57:27 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,24 @@ import { Hero } from "@/components/Hero";
 import { MainLaytout } from "@/components/MainLayout";
 import Head from "next/head";
 
+// import AwesomeSlider from "react-awesome-slider";
+// import AwsSliderStyles from "react-awesome-slider/src/styles.scss";
 export default function Home() {
   return (
     <main>
-
-    <Head>
-      <meta property="og:title" content="Badral's Blog"/>
-      <meta property="og:description" content="Badral Blog's description"/>
-      <meta property="og:image" content="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"/>
-      <meta property="og:type" content="website"/>
-    </Head>
+      <Head>
+        <meta property="og:title" content="Badral's Blog" />
+        <meta property="og:description" content="Badral Blog's description" />
+        <meta
+          property="og:image"
+          content="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"
+        />
+        <meta property="og:type" content="website" />
+      </Head>
       <MainLaytout className="px-5 md:px-0">
-
         <Header />
         <Hero />
-
       </MainLaytout>
-
     </main>
-  )
+  );
 }
-
