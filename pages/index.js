9ab8595628284b5515d60ac29d59ffcd8c2b6b0f
Badralbb <badralbaigalmaa7@gmail.com>
2024-08-05 18:41:56 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,26 @@ import { Header } from "@/components/Header";
 import { Footer, HeaderNav } from "@/components/HeaderNav";
 import { Hero } from "@/components/Hero";
 import { MainLaytout } from "@/components/MainLayout";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <MainLaytout className="px-5 md:px-0">
+    <main>
 
-      <Header />
-      <Hero />
+    <Head>
+      <meta property="og:title" content="Badral's Blog"/>
+      <meta property="og:description" content="Badral Blog's description"/>
+      <meta property="og:image" content="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"/>
+      <meta property="og:type" content="website"/>
+    </Head>
+      <MainLaytout className="px-5 md:px-0">
 
-    </MainLaytout>
+        <Header />
+        <Hero />
+
+      </MainLaytout>
+
+    </main>
   )
 }
 
