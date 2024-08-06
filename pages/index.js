b920e0ea0569be81b6721955d97b1b788e4269b1
Badralbb <badralbaigalmaa7@gmail.com>
2024-08-06 16:54:53 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,6 @@ import Head from "next/head";
 export default function Home() {
   return (
     <main>
-      <Head>
-        <meta property="og:title" content="Badral's Blog" />
-        <meta property="og:description" content="Badral Blog's description" />
-        <meta
-          property="og:image"
-          content="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"
-        />
-        <meta property="og:type" content="website" />
-      </Head>
       <MainLaytout className="px-5 md:px-0">
         <Header />
         <Hero />
