a48d0103f89cdd301f1b67bf91376681898ea71f
Badralbb <badralbaigalmaa7@gmail.com>
2024-09-03 21:12:52 +0800

    Update page.js

--- app/[author]/[slug]/page.js
+++ app/[author]/[slug]/page.js
@@ -22,9 +22,9 @@ export default async function Page({ params }) {
     const article = await response.json()
     return (
         <main>
-            <div className="prose w-[80%] mx-auto">
-                <div>{parse(article.title)}</div>
-                <div>{parse(article.body_html)}</div>
+            <div className="w-[80%] mx-auto">
+                <div>{article.title}</div>
+                <div>{article.body_html}</div>
             </div>
         </main>
     )
